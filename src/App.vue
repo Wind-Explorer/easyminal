33b5e983fa75d2bfdaf2ccc952aa7c9976f206e9
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-08-19 19:26:22 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Scripts for the component
+</script>
+
+<template>
+  <div class="container">
+    <!-- HTML elements for the component -->
+    <h1>Hello world :3</h1>
+  </div>
+</template>
+
+<style scoped>
+/* CSS styles for the component */
+</style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// Scripts for the component
+</script>
+
+<template>
+  <div class="container">
+    <!-- HTML elements for the component -->
+  </div>
+</template>
+
+<style scoped>
+/* CSS styles for the component */
+</style>
