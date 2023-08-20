ba2d2f130fd060ea4d39262b99df70536c745bb2
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2023-08-20 23:08:49 +0800

    Refactored terminal setup code

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -5,44 +5,45 @@ import { FitAddon } from "xterm-addon-fit";
 import "xterm/css/xterm.css";
 import { invoke } from "@tauri-apps/api";
 import { Event, listen } from "@tauri-apps/api/event";
+import { onMounted } from "vue";
 
-window.addEventListener('DOMContentLoaded', () => {
-  const terminalElement = document.getElementById("terminal") as HTMLElement;
-  
-  const fitAddon = new FitAddon();
-  const term = new Terminal({
-    fontFamily: "Jetbrains Mono",
-    theme: {
-      background: "rgb(47, 47, 47)",
-    }
+const fitAddon = new FitAddon();
+const term = new Terminal({
+  fontFamily: "Jetbrains Mono",
+  theme: {
+    background: "rgb(47, 47, 47)",
+  }
+});
+term.loadAddon(fitAddon);
+
+// Make the terminal fit all the window size
+function fitTerminal() {
+  fitAddon.fit();
+  void invoke<string>("async_resize_pty", {
+    rows: term.rows,
+    cols: term.cols,
   });
-  term.loadAddon(fitAddon);
+}
+
+// Write data from pty into the terminal
+function writeToTerminal(ev: Event<string>) {
+  term.write(ev.payload)
+}
+
+// Write data from the terminal to the pty
+function writeToPty(data: string) {
+  void invoke("async_write_to_pty", {
+    data,
+  });
+}
+term.onData(writeToPty);
+window.addEventListener("resize", fitTerminal);
+listen("data", writeToTerminal)
+fitTerminal();
+
+onMounted(() => {
+  const terminalElement = document.getElementById("terminal") as HTMLElement;
   term.open(terminalElement);
-
-  // Make the terminal fit all the window size
-  function fitTerminal() {
-    fitAddon.fit();
-    void invoke<string>("async_resize_pty", {
-      rows: term.rows,
-      cols: term.cols,
-    });
-  }
-
-  // Write data from pty into the terminal
-  function writeToTerminal(ev: Event<string>) {
-    term.write(ev.payload)
-  }
-
-  // Write data from the terminal to the pty
-  function writeToPty(data: string) {
-    void invoke("async_write_to_pty", {
-      data,
-    });
-  }
-  term.onData(writeToPty);
-  window.addEventListener("resize", fitTerminal);
-  listen("data", writeToTerminal)
-  fitTerminal();
 })
 </script>
 
